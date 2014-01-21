7f2e3e9186999c63417afafa6246d7e39332cf69
zsim0n <zoltan@zoltansimon.me>
2014-01-21 01:10:48 +0100

    Lesson #3: function run_bar()

---
+++ 3-function-run/pakefile.php
@@ -0,0 +1,18 @@
+<?php
+
+pake_task('bar');
+
+/**
+ * Callback / Code behind function for task "bar"
+ * 
+ * @param type $task PakeTask object self
+ * @param type $args Command line arguments
+ * @param type $options Command line Options
+ */
+function run_bar($task, $args, $options) {
+
+/**
+Task implementation goes here
+*/
+
+}
