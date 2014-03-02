2a8c5d1d9cb451439bac5a96ae9cbfda2ff8e887
zsim0n <zoltan@zoltansimon.me>
2014-03-02 21:55:44 +0100

    fix puppet manifest

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,58 +1,61 @@
 Exec {
-    path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
 }
-
+# install php stack
 class dev_box {
+  exec { 'apt_update':
+    command => 'apt-get -y update',
+    unless  => "test -e ${::home}/.rvm"
+  }
 
-    exec {
-        'apt_update': command => 'apt-get -y update',
-        unless => "test -e ${home}/.rvm"
-    }
+  Package {
+    ensure => 'installed'
+  }
 
-    Package {
-        ensure => "installed"
-    }
+  $enhancements = ['language-pack-da', 'php5', 'nodejs', 'mc', 'git']
 
-    $enhancements = ["language-pack-da", "php5", "nodejs", "mc", "git"]
+  package { 'enhancements':
+    name => $enhancements
+  }
 
-    package {
-        'enhancements': name => $enhancements
-    }
+  file { 'var_www':
+    ensure  => 'link',
+    path    => '/var/www',
+    target  => '/vagrant',
+    force   => true,
+  }
+  Exec['apt_update'] -> Package['enhancements'] -> File['var_www']
+}
+# add config php
+define phpconf ( $content, $filename = $title) {
+  file { $filename:
+    ensure  => present,
+    path    => "/etc/php5/mods-available/${filename}",
+    owner   => root,
+    group   => root,
+    mode    => '0444',
+    content => $content,
+  }
 
-    file {
-        'var_www': path => '/var/www',
-        ensure => 'link',
-        target => '/vagrant',
-        force => true,
-    }
-    Exec['apt_update'] -> Package['enhancements'] -> File['var_www']
+  file { 'etc_php5_cli':
+    ensure  => 'link',
+    path    => "/etc/php5/cli/conf.d/30-${filename}",
+    target  => "/etc/php5/mods-available/${filename}",
+    force   => true,
+  }
+
+  file { 'etc_php5_apache2':
+    ensure  => 'link',
+    path    => "/etc/php5/apache2/conf.d/30-${filename}",
+    target  => "/etc/php5/mods-available/${filename}",
+    force   => true,
+  }
+  File[$filename] -> File['etc_php5_cli'] -> File['etc_php5_apache2']
 }
 
-class add_php_ini($filename, $content) {
-    file {
-        'ini_file': path => "/etc/php5/mods-available/${filename}",
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 444,
-        content => $content,
-    }
-
-    define phpsymlink {
-        file {
-            $etc_path: path => "${etc_path}/conf.d/30-${filename}",
-            target => '/etc/php5/mods-available/${filename}',
-            ensure => 'link',
-            force => true,
-        }
-
-        File['ini_file'] -> Phpsymlink{['/etc/php5/cli', '/etc/php5/apache2']}
-    }
-}
-
-
-class {'dev_box': stage => 'main'} ->
-class {'add_php_ini': 
-            filename => 'phar.ini',
-            content => "phar.readonly = off \n"
+class {'dev_box':
+  stage => 'main'
+} ->
+phpconf { 'phar.ini':
+  content   => "phar.readonly = off \n"
 }
